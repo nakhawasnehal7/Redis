1a09ee41b9ac49bdce2b5d0da2357f288d324dbc
nakhawa <nakhawasnehal7@gmail.com>
2026-07-26 19:55:13 -0400

    added user database repository class

---
+++ src/main/java/com/example/rediscache/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.rediscache.repository;
+
+import com.example.rediscache.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
